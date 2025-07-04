0f185f4000237b026fcdafa5825c64b57b85ee14
parkjin21004 <parkjin21004@gmail.com>
2025-07-05 03:11:39 +0900

    update: 서버 프록시 설정 제거

--- vite.config.ts
+++ vite.config.ts
@@ -5,12 +5,4 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react(), tailwindcss()],
-    server: {
-        proxy: {
-            "/api": {
-                target: "https://skkusoftware.kr",
-                changeOrigin: true,
-            },
-        },
-    },
 });
